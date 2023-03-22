578c8ce445b064d05c3d9c726f8df5f43769af0e
tasbire.saiyera <tasbire.saiyera@cgi.com>
2023-03-22 09:58:43 -0600

    testing now

---
+++ BackEnd/CompanyService/CompanyService/Repository/Company.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations; 
+
+namespace CompanyService.Repository
+{
+    public class Company
+    {
+        [Key]
+        public int companyId { get; set; }
+        public string email { get; set; }
+        public string companyName { get; set; }
+        public string location { get; set; }
+        public string logo { get; set; }
+        public string contactDetails { get; set; }
+        public bool isApproved { get; set; }
+    }
+}
