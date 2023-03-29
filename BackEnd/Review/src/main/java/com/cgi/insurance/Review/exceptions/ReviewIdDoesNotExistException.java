7f1b74b4f44b2e3651df6b88e8aa1856ff29a6e4
pavithra.gopalakrishnan <pavithra.gopalakrishnan@cgi.com>
2023-03-29 11:12:40 -0400

    Added review service

---
+++ BackEnd/Review/src/main/java/com/cgi/insurance/Review/exceptions/ReviewIdDoesNotExistException.java
@@ -0,0 +1,9 @@
+package com.cgi.insurance.Review.exceptions;
+
+public class ReviewIdDoesNotExistException extends Exception {
+	
+	public ReviewIdDoesNotExistException(String message) {
+		super(message);
+	}
+
+}
