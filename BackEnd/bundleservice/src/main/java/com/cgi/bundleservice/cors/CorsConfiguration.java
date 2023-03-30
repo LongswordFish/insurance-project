a896824379bd34a25e4f221e73cd46f2f92fc843
vipul.darji <vipul.darji@cgi.com>
2023-03-30 10:51:33 -0400

    updated bundle service and pushed auth into backend

---
+++ BackEnd/bundleservice/src/main/java/com/cgi/bundleservice/cors/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package com.cgi.bundleservice.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+    	 registry.addMapping("/bundles/**")
+         .allowedOrigins("http://localhost:4200")
+         .allowedMethods("GET", "POST", "PUT", "DELETE")
+         .allowedHeaders("*");
+    }
+}
+
