62b3604f7d24f25025981170f62612f48c95b3a2
robin.yu <robin.yu@cgi.com>
2023-04-04 11:14:09 -0400

    navbar auto changes when user login

--- FrontEnd/insuranceApp/src/app/authentication/services/authenticate.service.ts
+++ FrontEnd/insuranceApp/src/app/authentication/services/authenticate.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { User } from '../model/User.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticateService {
+  user:User;
+  userSubject:BehaviorSubject<User>
 
   constructor(private http:HttpClient,private router:Router) {
-    
+    this.user={};
+    this.userSubject=new BehaviorSubject<User>({});
    }
+
    login(data:any){
     return this.http.post("http://localhost:9000/api/Auth/login",data);
     
@@ -21,6 +27,7 @@ export class AuthenticateService {
         sessionStorage.removeItem("Userid");
         sessionStorage.removeItem("role");
         sessionStorage.clear();
+        this.updateUserForNavbar({});
         this.router.navigate(['']);
     }
    }
@@ -28,4 +35,14 @@ export class AuthenticateService {
    register(data:any){
     return this.http.post("http://localhost:9000/api/Auth/register",data);
    }
+
+   updateUserForNavbar(user:User){
+    this.user=user;
+    this.userSubject.next(this.user);
+   }
+
+   getUserSubject():Observable<any>{
+    return this.userSubject;
+  }
+
 }
