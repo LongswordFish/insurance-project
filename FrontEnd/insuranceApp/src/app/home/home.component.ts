976811dd070bde10d458f294df2a214750324a56
vipul.darji <vipul.darji@cgi.com>
2023-04-05 18:50:13 -0400

    updated home

--- FrontEnd/insuranceApp/src/app/home/home.component.ts
+++ FrontEnd/insuranceApp/src/app/home/home.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  slides = [
+    { image: 'assets/images/slide1.jpg', alt: 'Slide 1' },
+    { image: 'assets/images/slide2.jpg', alt: 'Slide 2' },
+    { image: 'assets/images/slide3.jpg', alt: 'Slide 3' },
+  ];
 }
